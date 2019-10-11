f393c4cbed39a36f6f65fce0e24b160f0772c6a4
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2019-10-11 20:46:43 +0200

    Updates

--- scripts/script-jp.js
+++ scripts/script-jp.js
@@ -1,143 +1,83 @@
-// // Script to open and close sidebar
-// function w3_open() {
-//   document.getElementById("mySidebar").style.display = "block";
-//   document.getElementById("myOverlay").style.display = "block";
-// }
+// Script to open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
 
-// function w3_close() {
-//   document.getElementById("mySidebar").style.display = "none";
-//   document.getElementById("myOverlay").style.display = "none";
-// }
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
 
-// // Modal Image Gallery
-// function onClick(element) {
-//   document.getElementById("img01").src = element.src;
-//   document.getElementById("modal01").style.display = "block";
-//   var captionText = document.getElementById("caption");
-//   captionText.innerHTML = element.alt;
-// }
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
 
-// // The 'Contact details' button handler
-// document.addEventListener("DOMContentLoaded", () => {
-//   const officeAddress = "83 Cecil Road, Selly Park, Birmingham B29 7QQ";
-//   const emailAddress = "m.and.y.translations@gmail.com";
-//   const phoneNumber = "+44 780 9905";
-//   const INITIAL = "XXX";
-//   const SEE_DETAILS = "Click to see contact details";
-//   const HIDE_DETAILS = "Click to hide contact details";
+const officeAddress = "83 Cecil Road, Selly Park, Birmingham B29 7QQ";
+const emailAddress = "m.and.y.translations@gmail.com";
+const phoneNumber = "+44 780 9905";
+const INITIAL = "XXX";
+const SEE_DETAILS = "Click to see contact details";
+const SEE_DETAILS_SHORT = "Contact details";
+const HIDE_DETAILS = "Click to hide contact details";
+const HIDE_DETAILS_SHORT = "Hide details";
+const detailsButton = document.getElementById("contact-details-btn");
+const body = document.getElementsByTagName("body")[0];
+const WIDTH_LIMIT = 750;
+const detailsBox = document.getElementById("contact-details");
+const DETAILS_BOX_WIDTH = Number(
+  window
+    .getComputedStyle(detailsBox, null)
+    .getPropertyValue("width")
+    .substr(0, 3)
+);
+let detailsShown = false;
 
-//   let detailsShown = false;
+detailsButton.innerText =
+  DETAILS_BOX_WIDTH > WIDTH_LIMIT ? SEE_DETAILS : SEE_DETAILS_SHORT;
 
-//   const contactDetails = [
-//     document.getElementById("contact-office-address"),
-//     document.getElementById("contact-email-address"),
-//     document.getElementById("contact-telephone-number")
-//   ];
+body.onresize = () => handleOnResize();
 
-//   const detailsButton = document.getElementById("contact-details-btn");
-//   detailsButton.innerText = SEE_DETAILS;
-//   detailsButton.onclick = () => toggleDetails();
-
-//   contactDetails.forEach(item => (item.innerText = INITIAL));
-
-//   const toggleDetails = () => {
-//     detailsShown = !detailsShown;
-//     detailsButton.innerText = !detailsShown ? SEE_DETAILS : HIDE_DETAILS;
-
-//     contactDetails[0].innerText = detailsShown ? officeAddress : INITIAL;
-//     contactDetails[1].innerHTML = detailsShown
-//       ? `<a href="mailto:${emailAddress}" target="_top">${emailAddress}</a>`
-//       : INITIAL;
-//     contactDetails[2].innerText = detailsShown ? phoneNumber : INITIAL;
-//   };
-// });
-
-document.addEventListener("DOMContentLoaded", () => {
-  // Script to open and close sidebar
-  function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-  }
-
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-  }
-
-  // Modal Image Gallery
-  function onClick(element) {
-    document.getElementById("img01").src = element.src;
-    document.getElementById("modal01").style.display = "block";
-    var captionText = document.getElementById("caption");
-    captionText.innerHTML = element.alt;
-  }
-
-  const officeAddress = "83 Cecil Road, Selly Park, Birmingham B29 7QQ";
-  const emailAddress = "m.and.y.translations@gmail.com";
-  const phoneNumber = "+44 780 9905";
-  const INITIAL = "XXX";
-  const SEE_DETAILS = "Click to see contact details";
-  const SEE_DETAILS_SHORT = "Contact details";
-  const HIDE_DETAILS = "Click to hide contact details";
-  const HIDE_DETAILS_SHORT = "Hide details";
-  const detailsButton = document.getElementById("contact-details-btn");
-  const body = document.getElementsByTagName("body")[0];
-  const WIDTH_LIMIT = 750;
-  const detailsBox = document.getElementById("contact-details");
+const handleOnResize = () => {
   const DETAILS_BOX_WIDTH = Number(
     window
       .getComputedStyle(detailsBox, null)
       .getPropertyValue("width")
       .substr(0, 3)
   );
-  let detailsShown = false;
-
-  console.log(DETAILS_BOX_WIDTH, WIDTH_LIMIT);
 
   detailsButton.innerText =
     DETAILS_BOX_WIDTH > WIDTH_LIMIT ? SEE_DETAILS : SEE_DETAILS_SHORT;
+};
 
-  body.onresize = () => handleOnResize();
+// The 'Contact details' button handler
+const contactDetails = [
+  document.getElementById("contact-office-address"),
+  document.getElementById("contact-email-address"),
+  document.getElementById("contact-telephone-number")
+];
 
-  const handleOnResize = () => {
-    const DETAILS_BOX_WIDTH = Number(
-      window
-        .getComputedStyle(detailsBox, null)
-        .getPropertyValue("width")
-        .substr(0, 3)
-    );
-    console.log(DETAILS_BOX_WIDTH);
+detailsButton.onclick = () => toggleDetails();
 
-    detailsButton.innerText =
-      DETAILS_BOX_WIDTH > WIDTH_LIMIT ? SEE_DETAILS : SEE_DETAILS_SHORT;
-  };
+contactDetails.forEach(item => (item.innerText = INITIAL));
 
-  // The 'Contact details' button handler
-  const contactDetails = [
-    document.getElementById("contact-office-address"),
-    document.getElementById("contact-email-address"),
-    document.getElementById("contact-telephone-number")
-  ];
+const toggleDetails = () => {
+  detailsShown = !detailsShown;
+  detailsButton.innerText = !detailsShown
+    ? DETAILS_BOX_WIDTH > WIDTH_LIMIT
+      ? SEE_DETAILS
+      : SEE_DETAILS_SHORT
+    : DETAILS_BOX_WIDTH > WIDTH_LIMIT
+    ? HIDE_DETAILS
+    : HIDE_DETAILS_SHORT;
 
-  detailsButton.onclick = () => toggleDetails();
-
-  contactDetails.forEach(item => (item.innerText = INITIAL));
-
-  const toggleDetails = () => {
-    console.log("Toggling details", DETAILS_BOX_WIDTH);
-    detailsShown = !detailsShown;
-    detailsButton.innerText = !detailsShown
-      ? DETAILS_BOX_WIDTH > WIDTH_LIMIT
-        ? SEE_DETAILS
-        : SEE_DETAILS_SHORT
-      : DETAILS_BOX_WIDTH > WIDTH_LIMIT
-      ? HIDE_DETAILS
-      : HIDE_DETAILS_SHORT;
-
-    contactDetails[0].innerText = detailsShown ? officeAddress : INITIAL;
-    contactDetails[1].innerHTML = detailsShown
-      ? `<a href="mailto:${emailAddress}" target="_top">${emailAddress}</a>`
-      : INITIAL;
-    contactDetails[2].innerText = detailsShown ? phoneNumber : INITIAL;
-  };
-});
+  contactDetails[0].innerText = detailsShown ? officeAddress : INITIAL;
+  contactDetails[1].innerHTML = detailsShown
+    ? `<a href="mailto:${emailAddress}" target="_top">${emailAddress}</a>`
+    : INITIAL;
+  contactDetails[2].innerText = detailsShown ? phoneNumber : INITIAL;
+};
