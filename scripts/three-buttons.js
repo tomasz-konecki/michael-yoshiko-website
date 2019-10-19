9b8c6d1db2e1444710baca6880bf1f031a9d6140
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2019-10-19 23:26:58 +0200

    Updates

--- scripts/three-buttons.js
+++ scripts/three-buttons.js
@@ -1,4 +1,4 @@
-// Fields
+// Text Fields
 const commercialAddress = document.getElementById("commercial-address");
 const commercialTelephone = document.getElementById("commercial-telephone");
 const commercialEmail = document.getElementById("commercial-email");
@@ -8,11 +8,18 @@ const fields = [commercialAddress, commercialTelephone, commercialEmail];
 fields.forEach(field => (field.innerText = INITIAL));
 
 // Buttons
-const commercialAddressBtn = document.getElementById("commercial-address-btn");
-const commercialTelephoneBtn = document.getElementById(
-  "commercial-telephone-btn"
-);
-const commercialEmailBtn = document.getElementById("commercial-email-btn");
+const SHOW = "表示する";
+const HIDE = "表示を閉じる";
+
+btnIDs = {
+  address: "commercial-address-btn",
+  telephone: "commercial-telephone-btn",
+  email: "commercial-email-btn"
+};
+
+const commercialAddressBtn = document.getElementById(btnIDs.address);
+const commercialTelephoneBtn = document.getElementById(btnIDs.telephone);
+const commercialEmailBtn = document.getElementById(btnIDs.email);
 
 const buttons = [
   commercialAddressBtn,
@@ -20,19 +27,39 @@ const buttons = [
   commercialEmailBtn
 ];
 
-const SHOW = "Show";
-const HIDE = "Hide";
-
-let addressFlag = false;
-let telephoneFlag = false;
-let emailFlag = false;
-
-const toggleCommercialDetails = id => {
-  console.log("You clicke btn:", id);
-};
-
 buttons.forEach(btn => {
   btn.innerText = SHOW;
-  console.log(btn.id);
   btn.onclick = () => toggleCommercialDetails(btn.id);
 });
+
+// Flags
+const ADDRESS_FLAG = "addressFlag";
+const TELEPHONE_FLAG = "telephoneFlag";
+const EMAIL_FLAG = "emailFlag";
+
+let flags = {
+  [ADDRESS_FLAG]: false,
+  [TELEPHONE_FLAG]: false,
+  [EMAIL_FLAG]: false
+};
+
+const toggleCommercialDetails = id => {
+  const { address, telephone, email } = btnIDs;
+
+  switch (id) {
+    case address:
+      showDetails({ flag: ADDRESS_FLAG, index: 0, text: officeAddress });
+      break;
+    case telephone:
+      showDetails({ flag: TELEPHONE_FLAG, index: 1, text: phoneNumber });
+      break;
+    case email:
+      showDetails({ flag: EMAIL_FLAG, index: 2, text: emailAddress });
+  }
+};
+
+const showDetails = ({ flag, index, text }) => {
+  flags[flag] = !flags[flag];
+  buttons[index].innerText = flags[flag] ? HIDE : SHOW;
+  fields[index].innerText = flags[flag] ? text : INITIAL;
+};
